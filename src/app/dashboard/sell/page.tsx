bd82659b6ce5f42a5aea8fb92433ee24e1cb3477
akechi0523 <ericlee1994523@gmail.com>
2024-06-11 19:46:37 +1000

    Metadata updated

--- src/app/dashboard/sell/page.tsx
+++ src/app/dashboard/sell/page.tsx
@@ -1,13 +1,6 @@
 
-import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import BuyAndSellButton from "@/components/Dashboard/Buy/BuyAndSellButton";
-
-export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
-};
 
 export default function Home() {
   return (
