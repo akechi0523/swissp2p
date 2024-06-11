9cef7efa9220b119e88d74ed7d85d673f2de3d98
akechi0523 <ericlee1994523@gmail.com>
2024-06-12 01:22:53 +1000

    Buy with wallet

---
+++ src/app/dashboard/buy/wallet/revolut/page.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import Image from "next/image";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import BuyAndSellButton from "@/components/Dashboard/Buy/BuyAndSellButton";
+import Select from "@/components/Dashboard/Buy/Select1";
+import ReviewTransaction from "@/components/Dashboard/Buy/ReviewTransaction";
+import PaymentDetailSelect from "@/components/common/PaymentDetailSelect";
+import QRCode from "@/components/common/ORCode";
+import PaymentReceipt from "@/components/Dashboard/Buy/PaymentReceipt";
+
+
+export default function Bank() {
+  return (
+    <>
+      <DefaultLayout>
+        <div className="pr-16">
+          <div className="font-medium text-xl text-primary mb-5">Overview</div>
+          <BuyAndSellButton />
+          <div className="flex gap-8">
+            <div className="flex w-1/2 flex-col gap-8">
+              {/*Selct */}
+              <Select />
+              {/*Receiving Method */}
+              <div className="bg-white rounded-3xl px-8 py-6">
+                <div className="font-medium text-xl text-primary mb-3">Receiving Method</div>
+                <div className="flex flex-col gap-4">
+                  <div className='flex items-center justify-center gap-3 bg-third text-base text-primary font-bold rounded-2xl border-none py-3'>
+                    <Image src='/images/icon/icon-wallet-secondary.svg' width={36} height={36} alt='Wallet Deposit'/>
+                    Wallet Deposit
+                  </div>
+                </div>
+              </div>
+              {/*Payment Method */}
+              <div className="bg-white rounded-3xl px-8 py-6">
+                <div className="font-medium text-xl text-primary mb-3">Payment Method</div>
+                <div className="flex flex-col gap-3">
+                  <div className={`flex items-center gap-3 text-primary font-medium rounded-2xl bg-third border-[1px] border-[#E6E8EC] hover:bg-[#E6E8EC] px-6 py-3`}>
+                    <Image src='/images/icon/icon-revolut.svg' width={36} height={36} alt='Revolut'/>
+                    Revolut
+                  </div>
+                </div>
+              </div>
+              <div className="text-base text-primary">Description
+                <p className="text-sm text-secondary py-2">Lorem ipsum dolor sit amet consectetur. Erat donec etiam id dictum id ac. Vulputate egestas id lacus mauris lectus hac. Nunc semper malesuada sed pharetra tellus vitae.</p>
+              </div>
+            </div>
+            
+            <div className="flex w-1/2 flex-col gap-8">
+              {/*Payment Details */}
+              <div className="bg-white rounded-3xl px-8 py-6">
+                <div className="font-medium text-xl text-primary mb-3">Payment Details</div>
+                <div className="flex flex-col gap-2">
+                  <PaymentDetailSelect title="Full Name" placeholder="Account holder name"/>
+                  <PaymentDetailSelect title="Phone Number" placeholder="54536636635"/>
+                  <PaymentDetailSelect title="Tag Name" placeholder="@name"/>
+                </div>
+              </div>
+              <QRCode />
+              <PaymentReceipt />
+              <ReviewTransaction />
+            </div>
+          </div>
+        </div>
+      </DefaultLayout>
+    </>
+  );
+}
--- src/components/Dashboard/Buy/ReceivingMethod.tsx
+++ src/components/Dashboard/Buy/ReceivingMethod.tsx
@@ -10,33 +10,57 @@ const ReceivingMethod = () => {
   }
 
   let route ;
-  switch(paymentMethod) {
-    case "Bank Transfer":
-      route = 'buy/binance/bank';
-      break;
-    case "Twint":
-      route = 'buy/binance/twint'
-      break;
-    case "Zen":
-      route = 'buy/binance/zen';
-      break;
-    case "Wise":
-      route = 'buy/binance/wise';
-      break;
-    case "Revolut":
-      route = 'buy/binance/revolut';
-      break;
-    default:
-      route = "#";
-      break;
+  if (receivingMethod == 'Binance Pay') {
+    switch(paymentMethod) {
+      case "Bank Transfer":
+        route = 'buy/binance/bank';
+        break;
+      case "Twint":
+        route = 'buy/binance/twint'
+        break;
+      case "Zen":
+        route = 'buy/binance/zen';
+        break;
+      case "Wise":
+        route = 'buy/binance/wise';
+        break;
+      case "Revolut":
+        route = 'buy/binance/revolut';
+        break;
+      default:
+        route = "#";
+        break;
+    }
+  } else {
+    switch(paymentMethod) {
+      case "Bank Transfer":
+        route = 'buy/wallet/bank';
+        break;
+      case "Twint":
+        route = 'buy/wallet/twint'
+        break;
+      case "Zen":
+        route = 'buy/wallet/zen';
+        break;
+      case "Wise":
+        route = 'buy/wallet/wise';
+        break;
+      case "Revolut":
+        route = 'buy/wallet/revolut';
+        break;
+      default:
+        route = "#";
+        break;
+    }
   }
+  
 
   return (
     <div className="bg-white rounded-3xl px-8 py-6">
       <div className="font-medium text-xl text-primary mb-3">Receiving Method</div>
       <div className="flex flex-col gap-4">
         <ReceivingSelect path={`${amount ? route : '#'}`} bgcolor="bg-[#F2C94C]" icon_size={36} icon_src="/images/icon/icon-binance.svg" content="Binance Pay" handleClick={() => handleReceivingSelect("Binance Pay")} />
-        <ReceivingSelect path="#" bgcolor="bg-[#2F80ED]" icon_size={36} icon_src="/images/icon/icon-wallet.svg" content="Wallet Deposit" handleClick={() => handleReceivingSelect("Wallet Deposit")} />
+        <ReceivingSelect path={`${amount ? route : '#'}`} bgcolor="bg-[#2F80ED]" icon_size={36} icon_src="/images/icon/icon-wallet.svg" content="Wallet Deposit" handleClick={() => handleReceivingSelect("Wallet Deposit")} />
       </div>
     </div>
   );
