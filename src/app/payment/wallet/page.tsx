c051e809ee8a5c531803dbb6ef6030e39a5de650
akechi0523 <ericlee1994523@gmail.com>
2024-06-13 11:10:53 +1000

    Payment using wallet

--- src/app/payment/wallet/page.tsx
+++ src/app/payment/wallet/page.tsx
@@ -5,6 +5,8 @@ import Account from "@/components/Payment/Account";
 import ConfirmButton from "@/components/common/ConfirmButton";
 import VerificationCode from "@/components/Payment/VerificaitionCode";
 import CommonComboBox from "@/components/common/CommonComboBox";
+import AddWallet from "@/components/Payment/AddWallet";
+import Image from "next/image";
 import { useState } from "react";
 
 const selectCurrencyOptions = [
@@ -31,7 +33,7 @@ export default function Buy() {
             <div className="flex gap-12">
               <div className="flex flex-col w-1/2 gap-8">
                 <Account icon_src1="/images/icon/icon-bank.svg" name="Wallet Address" icon_src2={`${!isConfirm ? "/images/icon/icon-checked2.svg" : "/images/icon/icon-checked.svg"}`}/>
-                {/*Payment Details */}
+                {/*Wallet Details */}
                 <div className="bg-white rounded-3xl px-8 py-6">
                   <div className="font-medium text-xl text-primary mb-3">Wallet Details</div>
                   <div className="flex flex-col">
@@ -39,7 +41,26 @@ export default function Buy() {
                     <CommonComboBox options={selectNetworkOptions} select="Select Network" isConfirm={isConfirm}/>
                   </div>
                 </div>
+                {/**Wallet Address */}
+                <div className="bg-white rounded-3xl px-8 py-6">
+                  <div className="font-medium text-xl text-primary mb-3">Wallet Address</div>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center justify-center bg-third text-secondary rounded-full border-none px-6 py-3">
+                      Wallet Address
+                    </div>
+                    <div className="flex items-center gap-2 font-medium text-primary">
+                      <Image src='/images/icon/icon-tron.svg' width={24} height={24} alt="icon"/>
+                      Tron
+                    </div>
+                    { !isConfirm &&
+                      <div className="text-base text-primary">Description
+                        <p className="text-sm text-secondary py-2">Lorem ipsum dolor sit amet consectetur. Erat donec etiam id dictum id ac. Vulputate egestas id lacus mauris lectus hac. Nunc semper malesuada sed pharetra tellus vitae.</p>
+                      </div>
+                    }
+                  </div>
+                </div>
                 {!isConfirm && <ConfirmButton handleClick={handleClick} />}
+                { isConfirm && <AddWallet />}
               </div>
               <div className="flex flex-col w-1/2 gap-8">
               { !isConfirm && 
---
+++ src/components/Payment/AddWallet.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const AddWallet = () => {
+  return (
+    <button className="flex items-center justify-center gap-3 text-base text-white font-bold rounded-2xl border-none py-4.5 bg-[#23262F]">
+      <Image src='/images/icon/icon-pluse.svg' width={18} height={18} alt="icon"/>
+      Add Receiving Wallet
+    </button>
+  );
+}
+
+export default AddWallet;
