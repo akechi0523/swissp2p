82a232fb2a52bd5700ff33715cb93c121486fe78
akechi0523 <ericlee1994523@gmail.com>
2024-06-15 03:48:49 +1000

    Dark mode updated

--- src/components/Header/WalletConnect.tsx
+++ src/components/Header/WalletConnect.tsx
@@ -34,7 +34,7 @@ const WalletConnect = () => {
       </button>
       {openModal &&
         <div className='absolute top-[80px] 2xl:right-19 md:right-14 w-full z-99999 flex md:justify-end justify-center right-0 px-6'>
-          <div ref={modalRef} className='flex flex-col gap-4 sm:w-125 bg-white rounded-3xl px-6 py-3 shadow-6 shadow-neutral-400'>
+          <div ref={modalRef} className='flex flex-col gap-4 sm:w-125 bg-white dark:bg-primary rounded-3xl px-6 py-3 shadow-6 shadow-neutral-400'>
             <div>
               <div className="font-medium text-xl text-primary dark:text-white mb-3">Connect Wallet</div>        
               <div className="font-normal text-base text-secondary">Choose how you want to connect. There are several 
@@ -43,7 +43,7 @@ const WalletConnect = () => {
             <WalletType icon='/images/icon/icon-metamask.svg' name='Metamask'/>     
             <WalletType icon='/images/icon/icon-walletconnect.svg' name='Wallet Connect'/>     
             <WalletType icon='/images/icon/icon-coinbase.svg' name='Coinbase Wallet'/>
-            <button className="flex items-center justify-center text-base font-medium text-primary dark:text-white rounded-full border-[1px] border-thrid dark:border-secondary px-6 py-3 mb-3 hover:bg-third">
+            <button className="flex items-center justify-center text-base font-medium text-primary dark:text-white rounded-full border-[1px] border-thrid dark:border-secondary px-6 py-3 mb-3 hover:bg-third dark:hover:bg-secondary">
               See More
             </button>     
           </div>
--- src/components/Header/WalletType.tsx
+++ src/components/Header/WalletType.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const WalletType = ({name, icon}: Props) => {
   return (
-    <button  className="flex items-center justify-between text-base font-medium text-primary dark:text-white rounded-full border-[1px] border-thrid dark:border-secondary px-6 py-3 hover:bg-third">
+    <button  className="flex items-center justify-between text-base font-medium text-primary dark:text-white rounded-full border-[1px] border-thrid dark:border-secondary px-6 py-3 hover:bg-third dark:hover:bg-secondary">
 			{name}
       <Image src={icon} width={24} height={24} alt="icon"/>
 		</button >
