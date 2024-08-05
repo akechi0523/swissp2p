36ac982313189c0c4910bf5035688f5dd8762ee4
akechi0523 <ericlee1994523@gmail.com>
2024-08-06 07:54:40 +1000

    edit wallet connect button

--- src/components/Header/WalletConnect.tsx
+++ src/components/Header/WalletConnect.tsx
@@ -6,32 +6,24 @@ import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 
 const WalletConnect = () => {
-  const [openModal, setModal] = useState(false);
-  const [openModalCoinBtn, setModalCoinBtn] = useState(false);
   const [openModalCoin, setModalCoin] = useState(false);
-  const [walletName, setwalletName] = useState('Connect Wallet');
+  const [walletName, setwalletName] = useState('Metamask');
   const [coinName, setCoinName] = useState('Ethereum');
   const modalRef = useRef(null);
 
   const handleModal = () => {
-    setModal(true);
     setModalCoin(false);
   }
   const handleModalCoin = () => {
     setModalCoin(true);
-    setModal(false);
   }
 
-  const handleClickOutside = () => {
-    // if (modalRef.current) {
-    //   setModal(false);
-    // }
-    setModal(false)
-  }
-  const setWallet = (name: string) => {
-    setwalletName(name);
-    handleClickOutside();
-    setModalCoinBtn(true);
+  const setWallet = () => {
+    if (coinName === 'Tron')
+      setwalletName('Tronlink')
+    else if (coinName === 'Solana')
+      setwalletName('Phantom')
+    else setwalletName('Metamask')
   }
   const setCoin = (name: string) => {
     setCoinName(name);
@@ -44,24 +36,26 @@ const WalletConnect = () => {
   //     document.removeEventListener('click', handleClickOutside);
   //   };
   // }, []);
+  useEffect(() => {
+    setWallet()
+  }, [coinName])
 
   return (
     <>
-      {openModalCoinBtn &&
-        <button
-          className={`px-4 flex py-2 rounded-xl bg-primary text-white text-sm ${coinName === 'Ethereum' ? 'from-[#a78bfa] to-[#6d28d9] bg-gradient-to-r' :
-            (coinName === 'Arbitrum' ? 'from-[#06b6d4] to-[#082f49] bg-gradient-to-r' :
-              (coinName === 'Optimism' ? 'from-[#4c0519] to-[#be123c] bg-gradient-to-r' :
-                (coinName === 'Base' ? 'from-[#172554] to-[#1d4ed8] bg-gradient-to-r' :
-                  (coinName === 'BNB Chain' ? 'from-[#ca8a04] to-[#fde68a] bg-gradient-to-r' :
-                    (coinName === 'Avalanche' ? 'from-[#4c0519] to-[#f43f5e] bg-gradient-to-r' :
-                      (coinName === 'Solana' ? 'from-green-400 to-[#7c3aed] bg-gradient-to-r' :
-                        (coinName === 'Tron' ? 'from-[#4c0519] to-[#be123c] bg-gradient-to-r' :
-                          'dark:bg-secondary'
-                        )))))))}`}
-          onClick={handleModalCoin}
-        >
-          {coinName != "Connect Coin" &&
+      <button
+        className={`px-4 flex py-2 rounded-xl bg-primary text-white text-sm ${coinName === 'Ethereum' ? 'from-[#a78bfa] to-[#6d28d9] bg-gradient-to-r' :
+          (coinName === 'Arbitrum' ? 'from-[#06b6d4] to-[#082f49] bg-gradient-to-r' :
+            (coinName === 'Optimism' ? 'from-[#4c0519] to-[#be123c] bg-gradient-to-r' :
+              (coinName === 'Base' ? 'from-[#172554] to-[#1d4ed8] bg-gradient-to-r' :
+                (coinName === 'BNB Chain' ? 'from-[#ca8a04] to-[#fde68a] bg-gradient-to-r' :
+                  (coinName === 'Avalanche' ? 'from-[#4c0519] to-[#f43f5e] bg-gradient-to-r' :
+                    (coinName === 'Solana' ? 'from-green-400 to-[#7c3aed] bg-gradient-to-r' :
+                      (coinName === 'Tron' ? 'from-[#4c0519] to-[#be123c] bg-gradient-to-r' :
+                        'dark:bg-secondary'
+                      )))))))}`}
+        onClick={handleModalCoin}
+      >
+        {coinName != "Connect Coin" &&
           <Image src={`${coinName === 'Ethereum' ? '/images/logo/ethereum_logo.svg' : (coinName === 'Arbitrum' ? '/images/logo/arbitrum-logo.svg' :
             (coinName === 'Optimism' ? '/images/logo/optimism-logo.svg' :
               (coinName === 'Base' ? '/images/logo/base-logo.svg' :
@@ -71,51 +65,29 @@ const WalletConnect = () => {
                       (coinName === 'Tron' ? '/images/logo/tron-logo.svg' :
                         ''
                       )))))))}`} width={17} height={17} alt="icon" className='mt-0.5 me-2' />}
-          {coinName}
-          <Image src={"/images/icon/arrow-down.svg"} width={12} height={12} alt="icon" className='mt-2 ms-2' />
-        </button>
-      }
+        {coinName}
+        <Image src={"/images/icon/arrow-down.svg"} width={12} height={12} alt="icon" className='mt-2 ms-2' />
+      </button>
+
       <button
-        className={`px-4 flex py-2 rounded-full bg-primary text-white text-sm ${walletName === 'Metamask' ? 'from-[#7c2d12] to-[#fb923c] bg-gradient-to-r' :
-          (walletName === 'Tronlink' ? 'from-[#1e40af] to-[#3b82f6] bg-gradient-to-r' :
-            (walletName === 'Phantom' ? 'from-[#5b21b6] to-[#a78bfa] bg-gradient-to-r' :
-              'dark:bg-secondary'
-            ))
+        className={`px-4 flex py-2 rounded-full bg-primary text-white text-sm ${coinName === 'Tron' ? 'from-[#1e40af] to-[#3b82f6] bg-gradient-to-r' :
+          (coinName === 'Solana' ? 'from-[#5b21b6] to-[#a78bfa] bg-gradient-to-r' :
+            'from-[#78350f] to-[#d97706] bg-gradient-to-r'
+          )
           }`}
         onClick={handleModal}
-      >
-        {walletName != "Connect Wallet" && <Image src={`${walletName === 'Metamask' ? '/images/icon/icon-metamask.svg' :
-          (walletName === 'Tronlink' ? '/images/icon/icon-wallet/icon-tronlink.svg' :
-            (walletName === 'Phantom' ? '/images/icon/icon-wallet/icon-phantom.svg' :
-              ''
-            ))}`} width={14} height={14} alt="icon" className='mt-1 me-2' />}
+      ><Image src={`${coinName === 'Tron' ? '/images/icon/icon-wallet/icon-tronlink.svg' :
+        (coinName === 'Solana' ? '/images/icon/icon-wallet/icon-phantom.svg' :
+          (coinName === 'Ethereum' ? '/images/icon/icon-metamask.svg' :
+            (coinName === 'Arbitrum' ? '/images/icon/icon-metamask.svg' :
+              (coinName === 'Optimism' ? '/images/icon/icon-metamask.svg' :
+                (coinName === 'Base' ? '/images/icon/icon-metamask.svg' :
+                  (coinName === 'BNB Chain' ? '/images/icon/icon-metamask.svg' :
+                    (coinName === 'Avalanche' ? '/images/icon/icon-metamask.svg' : ''
+                    )))))))}`} width={14} height={14} alt="icon" className='mt-1 me-2' />
         {walletName}
-        {walletName != "Connect Wallet" && <Image src={"/images/icon/arrow-down.svg"} width={12} height={12} alt="icon" className='mt-2 ms-2' />}
       </button>
-      {openModal &&
-        <div className='absolute top-[80px] 2xl:right-19 md:right-14 w-full z-99999 flex md:justify-end justify-center right-0 px-6'>
-          <div ref={modalRef} className='flex flex-col gap-4 sm:w-125 bg-white dark:bg-primary rounded-3xl px-6 py-3 shadow-6 shadow-neutral-400'>
-            <div>
-              <div className="font-medium text-xl text-primary dark:text-white mb-3">Connect Wallet</div>
-              <div className="font-normal text-base text-secondary">Choose how you want to connect. There are several
-                wallet providers.</div>
-            </div>
-            <div onClick={() => setWallet('Metamask')}>
-              <WalletType icon='/images/icon/icon-metamask.svg' name='Metamask' />
-            </div>
-            <div onClick={() => setWallet('Tronlink')}>
-              <WalletType icon='/images/icon/icon-wallet/icon-tronlink.svg' name='Tronlink' />
-            </div>
-            {/* <WalletType icon='/images/icon/icon-coinbase.svg' name='Coinbase Wallet' /> */}
-            <div onClick={() => setWallet('Phantom')}>
-              <WalletType icon='/images/icon/icon-wallet/icon-phantom.svg' name='Phantom' />
-            </div>
-            <button className="flex items-center justify-center text-base font-medium text-primary dark:text-white rounded-full border-[1px] border-thrid dark:border-secondary px-6 py-3 mb-3 hover:bg-third dark:hover:bg-secondary">
-              See More
-            </button>
-          </div>
-        </div>
-      }
+
       {openModalCoin &&
         <div className='absolute top-[80px] 2xl:right-19 md:right-24 w-full z-99999 flex md:justify-end justify-center right-0 px-6 mr-24'>
           <div ref={modalRef} className='flex flex-col gap-4 sm:w-50 bg-white dark:bg-primary rounded-3xl px-6 py-3 shadow-6 shadow-neutral-400'>
